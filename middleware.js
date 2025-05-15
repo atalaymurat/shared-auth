bb9cb5fc2763ca46ab9d9fa4a736277db6311a5c
atalaymurat <atalaymurat@msn.com>
2025-05-15 13:53:09 +0300

    authenticate mid remove user

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 const { verifyToken } = require("./token");
 
-const authenticate = (UserModel) => async (req, res, next) => {
+const authenticate = () => (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
@@ -9,19 +9,14 @@ const authenticate = (UserModel) => async (req, res, next) => {
 
     const token = authHeader.split(" ")[1];
     const decoded = verifyToken(token);
-    const user = await UserModel.findById(decoded.userId).select(
-      "_id email name profilePicture roles isActive createdAt preferences firebaseUid emailVerified"
-    );
 
-    if (!user || !user.isActive) {
-      return res.status(403).json({ error: "Forbidden" });
-    }
+    // UserModel sorgusu yapma, direkt decoded veriyi req.user olarak ata
+    req.user = decoded;
 
-    req.user = user;
     next();
   } catch (err) {
     return res.status(401).json({ error: "Invalid or expired token" });
   }
 };
 
-module.exports = authenticate;
+module.exports = { authenticate };
