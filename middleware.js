a101e64402f9cd4fe6ed026592982a7df7831a2a
atalaymurat <atalaymurat@msn.com>
2025-05-15 14:56:01 +0300

    export authenticate

--- middleware.js
+++ middleware.js
@@ -1,3 +1,5 @@
+const { verifyToken } = require("./token")
+
 const authenticate = () => (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
@@ -19,3 +21,5 @@ const authenticate = () => (req, res, next) => {
     return res.status(401).json({ error: "Invalid or expired token" });
   }
 };
+
+module.exports = authenticate;
