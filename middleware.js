f5c4f052193ad37bbbab8cbfa4e812c93aeeb18e
atalaymurat <atalaymurat@msn.com>
2025-05-15 15:00:39 +0300

    export obj

--- middleware.js
+++ middleware.js
@@ -1,15 +1,20 @@
-const { verifyToken } = require("./token")
+const { verifyToken } = require("./token");
 
 const authenticate = () => (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
       console.log("No authHeader or authHeader does not start with Bearer");
-      return res.status(401).json({ error: "Unauthorized No Authheader or Not Start with Bearer" });
+      return res
+        .status(401)
+        .json({ error: "Unauthorized No Authheader or Not Start with Bearer" });
     }
 
     const token = authHeader.split(" ")[1];
-    console.log("SHARED-AUTH PACKAGE authorization mid token bearer heder:", token);
+    console.log(
+      "SHARED-AUTH PACKAGE authorization mid token bearer heder:",
+      token
+    );
     const decoded = verifyToken(token);
     console.log("Decoded token:", decoded);
 
@@ -22,4 +27,4 @@ const authenticate = () => (req, res, next) => {
   }
 };
 
-module.exports = authenticate;
+module.exports = { authenticate };
